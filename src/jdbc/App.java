02c0e2b5b95bc8383dec3522aec133d5d4ade72c
Dru-blip <62353412+Dru-blip@users.noreply.github.com>
2024-07-02 23:08:58 +0530

    Authentication Exercise

--- src/jdbc/App.java
+++ src/jdbc/App.java
@@ -1,9 +1,12 @@
 package jdbc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 
 public class App {
@@ -15,22 +18,31 @@ public class App {
         String insertQuery = "insert into person values(?,?,?,?)";
         String updateQuery="update person set pname=? where id=?";
         String deleteQuery="delete from person  where id=?";
-
+        Scanner scanner=new Scanner(System.in);
 
         try {
-            PreparedStatement insert = connection.prepareStatement(updateQuery);
-            insert.setString(1,"Naruto");
-            insert.setInt(2,4);
-            insert.executeUpdate();
 
-            PreparedStatement delete=connection.prepareStatement(deleteQuery);
-            delete.setInt(1,6);
-            delete.executeUpdate();
+            for (int i = 1; i <11; i++) {
+                PreparedStatement insert = connection.prepareStatement(insertQuery);
+                insert.setInt(1,i);
+                System.out.print("Enter Name : ");
+                insert.setString(2,scanner.next());
+                System.out.print("Enter City : ");
+                insert.setString(3,scanner.next());
+                System.out.print("Enter Country : ");
+                insert.setString(4,scanner.next());
+                insert.executeUpdate();
+            }
+
+
+//            PreparedStatement delete=connection.prepareStatement(deleteQuery);
+//            delete.setInt(1,6);
+//            delete.executeUpdate();
 
             ResultSet set = connection.prepareStatement(query).executeQuery();
 
             while (set.next()) {
-                System.out.println(set.getInt(1) + " " + set.getString(2));
+                System.out.println(set.getInt(1) + "\t" + set.getString(2) + "\t"+set.getString(3));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
